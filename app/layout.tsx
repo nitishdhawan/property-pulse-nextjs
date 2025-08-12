ea11653f1ff70ff195334bca2a478b35946cad06
nitishdhawan <37472048+nitishdhawan@users.noreply.github.com>
2025-08-12 18:55:22 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import '@/assets/styles/global.css'
 
 export const metadata = {
-  title: 'Property Pulse',
+  title: 'Property Pulse abc',
   keywords: 'property management, real estate, property listings',
   description: 'A Next.js application for property management',
 }
